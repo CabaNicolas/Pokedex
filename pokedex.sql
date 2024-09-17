622f77ce2b4e6a7b895cffce6b6cf75280db3634
Caba Nicolas <ncaba127@alumno.unlam.edu.ar>
2024-09-17 11:52:31 -0300

    Correccion de restriccion en numero de pokemon como unico y datos de los mismos relacionados a los de pokedex

--- pokedex.sql
+++ pokedex.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2024 a las 05:58:19
+-- Tiempo de generación: 17-09-2024 a las 14:21:39
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `evolucion` (
-                             `id_poke` int(11) NOT NULL,
-                             `id_poke2` int(11) NOT NULL
+  `id_poke` int(11) NOT NULL,
+  `id_poke2` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -39,11 +39,11 @@ CREATE TABLE `evolucion` (
 --
 
 CREATE TABLE `pokemon` (
-                           `id` int(11) NOT NULL,
-                           `numero` int(11) NOT NULL,
-                           `nombre` varchar(40) NOT NULL,
-                           `imagen` text NOT NULL,
-                           `descripcion` text NOT NULL
+  `id` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `imagen` text NOT NULL,
+  `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -51,12 +51,12 @@ CREATE TABLE `pokemon` (
 --
 
 INSERT INTO `pokemon` (`id`, `numero`, `nombre`, `imagen`, `descripcion`) VALUES
-                                                                              (1, 1, 'Charmander', 'Imagenes/Charmander.webp', 'Charmander es un pequeño monstruo bípedo parecido a un lagarto. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola, cuya punta está envuelta en llamas. Estas llamas indican su fuerza vital. Si está débil, las llamas arderán más tenue.\r\n'),
-                                                                              (2, 2, 'Charmeleon', 'Imagenes/Charmeleon.webp', 'Charmeleon es un gran lagarto bípedo con escamas de color rojo oscuro y en la parte inferior de color crema, posee como característica general una llama en la punta de su cola al igual que Charmander y Charizard. Es un Pokemon orgulloso que ama las batallas, esgrime su cola para elevar la temperatura y buscando derribar al oponente.\r\n'),
-                                                                              (3, 3, 'Bulbasaur', 'Imagenes/Bulbasaur.webp', '\'Bulbasaur es un Pokémon tipo Planta/Veneno. Es conocido por tener una planta en su espalda que crece con él desde que nace.\''),
-                                                                              (4, 4, 'Ivysaur', 'Imagenes/Ivysaur.webp', '\'Ivysaur es la forma evolucionada de Bulbasaur. La planta en su espalda ha crecido más y ahora tiene un brote de flor.\''),
-                                                                              (5, 5, 'Squirtle', 'Imagenes/Squirtle.webp', '\'Squirtle es un Pokémon tipo Agua. Es una pequeña tortuga azul que utiliza su caparazón para defenderse de ataques y disparar agua con fuerza.\''),
-                                                                              (6, 6, 'Wartortle', 'Imagenes/Wartortle.webp', '\'Wartortle es la evolución de Squirtle. Tiene orejas peludas y su cola se ha vuelto más grande, lo que lo hace nadar con mayor agilidad.\'');
+(1, 4, 'Charmander', 'Imagenes/Charmander.webp', 'Charmander es un pequeño monstruo bípedo parecido a un lagarto. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola, cuya punta está envuelta en llamas. Estas llamas indican su fuerza vital. Si está débil, las llamas arderán más tenue.\r\n'),
+(2, 5, 'Charmeleon', 'Imagenes/Charmeleon.webp', 'Charmeleon es un gran lagarto bípedo con escamas de color rojo oscuro y en la parte inferior de color crema, posee como característica general una llama en la punta de su cola al igual que Charmander y Charizard. Es un Pokemon orgulloso que ama las batallas, esgrime su cola para elevar la temperatura y buscando derribar al oponente.\r\n'),
+(3, 1, 'Bulbasaur', 'Imagenes/Bulbasaur.webp', '\'Bulbasaur es un Pokémon tipo Planta/Veneno. Es conocido por tener una planta en su espalda que crece con él desde que nace.\''),
+(4, 2, 'Ivysaur', 'Imagenes/Ivysaur.webp', '\'Ivysaur es la forma evolucionada de Bulbasaur. La planta en su espalda ha crecido más y ahora tiene un brote de flor.\''),
+(5, 7, 'Squirtle', 'Imagenes/Squirtle.webp', '\'Squirtle es un Pokémon tipo Agua. Es una pequeña tortuga azul que utiliza su caparazón para defenderse de ataques y disparar agua con fuerza.\''),
+(6, 8, 'Wartortle', 'Imagenes/Wartortle.webp', '\'Wartortle es la evolución de Squirtle. Tiene orejas peludas y su cola se ha vuelto más grande, lo que lo hace nadar con mayor agilidad.\'');
 
 -- --------------------------------------------------------
 
@@ -65,8 +65,8 @@ INSERT INTO `pokemon` (`id`, `numero`, `nombre`, `imagen`, `descripcion`) VALUES
 --
 
 CREATE TABLE `tipo` (
-                        `id` int(11) NOT NULL,
-                        `nombre` varchar(20) NOT NULL
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -74,9 +74,9 @@ CREATE TABLE `tipo` (
 --
 
 INSERT INTO `tipo` (`id`, `nombre`) VALUES
-                                        (1, 'Imagenes/fuego.webp'),
-                                        (2, 'Imagenes/hierba.webp'),
-                                        (3, 'Imagenes/agua.webp');
+(1, 'Imagenes/fuego.webp'),
+(2, 'Imagenes/hierba.webp'),
+(3, 'Imagenes/agua.webp');
 
 -- --------------------------------------------------------
 
@@ -85,8 +85,8 @@ INSERT INTO `tipo` (`id`, `nombre`) VALUES
 --
 
 CREATE TABLE `tipo_pokemon` (
-                                `id_pokemon` int(11) NOT NULL,
-                                `id_tipo` int(11) NOT NULL
+  `id_pokemon` int(11) NOT NULL,
+  `id_tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -94,12 +94,12 @@ CREATE TABLE `tipo_pokemon` (
 --
 
 INSERT INTO `tipo_pokemon` (`id_pokemon`, `id_tipo`) VALUES
-                                                         (1, 1),
-                                                         (2, 1),
-                                                         (3, 2),
-                                                         (4, 2),
-                                                         (5, 3),
-                                                         (6, 3);
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 2),
+(5, 3),
+(6, 3);
 
 -- --------------------------------------------------------
 
@@ -108,9 +108,9 @@ INSERT INTO `tipo_pokemon` (`id_pokemon`, `id_tipo`) VALUES
 --
 
 CREATE TABLE `usuario` (
-                           `id` int(11) NOT NULL,
-                           `email` varchar(50) NOT NULL,
-                           `password` varchar(20) NOT NULL
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -121,33 +121,34 @@ CREATE TABLE `usuario` (
 -- Indices de la tabla `evolucion`
 --
 ALTER TABLE `evolucion`
-    ADD PRIMARY KEY (`id_poke`,`id_poke2`),
+  ADD PRIMARY KEY (`id_poke`,`id_poke2`),
   ADD KEY `Evolucion` (`id_poke2`);
 
 --
 -- Indices de la tabla `pokemon`
 --
 ALTER TABLE `pokemon`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_numero` (`numero`);
 
 --
 -- Indices de la tabla `tipo`
 --
 ALTER TABLE `tipo`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `tipo_pokemon`
 --
 ALTER TABLE `tipo_pokemon`
-    ADD KEY `Pokemon_FK` (`id_pokemon`),
+  ADD KEY `Pokemon_FK` (`id_pokemon`),
   ADD KEY `Tipo_FK` (`id_tipo`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -157,19 +158,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `pokemon`
 --
 ALTER TABLE `pokemon`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo`
 --
 ALTER TABLE `tipo`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -179,14 +180,14 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `evolucion`
 --
 ALTER TABLE `evolucion`
-    ADD CONSTRAINT `Evolucion` FOREIGN KEY (`id_poke2`) REFERENCES `pokemon` (`id`),
+  ADD CONSTRAINT `Evolucion` FOREIGN KEY (`id_poke2`) REFERENCES `pokemon` (`id`),
   ADD CONSTRAINT `Evolucion2` FOREIGN KEY (`id_poke`) REFERENCES `pokemon` (`id`);
 
 --
 -- Filtros para la tabla `tipo_pokemon`
 --
 ALTER TABLE `tipo_pokemon`
-    ADD CONSTRAINT `Pokemon_FK` FOREIGN KEY (`id_pokemon`) REFERENCES `pokemon` (`id`),
+  ADD CONSTRAINT `Pokemon_FK` FOREIGN KEY (`id_pokemon`) REFERENCES `pokemon` (`id`),
   ADD CONSTRAINT `Tipo_FK` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id`);
 COMMIT;
 
