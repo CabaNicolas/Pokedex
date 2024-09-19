f81babdbaef31b5b532c995246e7e48eed04dc24
Sofia <sofiapulice13@gmail.com>
2024-09-19 16:29:01 -0300

    Agregar y modificar Pokemon

--- src/Pokemon.php
+++ src/Pokemon.php
@@ -31,6 +31,32 @@ class Pokemon{
         return $pokemons;
     }
 
+    public function getPokemonById($id) {
+        $db = DB::getConexion();
+
+        $query = "
+        SELECT p.*, t.nombre AS tipo, 
+               ( SELECT e.id_poke2 
+                 FROM evolucion e 
+                 WHERE e.id_poke = p.id AND e.id_poke2 != p.id ) 
+                 AS evoluciones 
+        FROM pokemon p 
+        LEFT JOIN tipo_pokemon tp ON p.id = tp.id_pokemon 
+        LEFT JOIN tipo t ON tp.id_tipo = t.id 
+        WHERE p.id = :id
+        LIMIT 1;
+    ";
+
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $pokemon = $stmt->fetchObject('Pokemon');
+
+        return $pokemon ? $pokemon : null;
+    }
+
+
     public function getId(){
         return $this->id;
     }
@@ -113,4 +139,105 @@ class Pokemon{
             unlink($imagen);
         }
     }
+
+    public function createPokemon($nombre, $numero, $tipos, $imagen, $descripcion) {
+        $db = DB::getConexion();
+
+        // validar si el numero ya existe
+        $query = "SELECT COUNT(*) FROM pokemon WHERE numero = :numero";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(':numero', $numero);
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+
+        if ($count > 0) {
+            echo "Error: Ya existe un Pokémon con el número $numero.";
+            return;
+        }
+
+        $query = "INSERT INTO pokemon (nombre, numero, imagen, descripcion) 
+              VALUES (:nombre, :numero, :imagen, :descripcion)";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(':nombre', $nombre);
+        $stmt->bindValue(':numero', $numero);
+        $stmt->bindValue(':imagen', $imagen);
+        $stmt->bindValue(':descripcion', $descripcion);
+        $stmt->execute();
+
+        $pokemonId = $db->lastInsertId();
+
+        // verificar que los tipos existen en la tabla tipo
+        $query = "SELECT id FROM tipo WHERE id IN (" . implode(',', array_fill(0, count($tipos), '?')) . ")";
+        $stmt = $db->prepare($query);
+        $stmt->execute($tipos);
+        $validTypes = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+
+        // insertar los tipos asociados en la tabla tipo_pokemon
+        foreach ($tipos as $tipo) {
+            if (in_array($tipo, $validTypes)) { //verifica si el tipo es valido
+                $query = "INSERT INTO tipo_pokemon (id_pokemon, id_tipo) 
+                      VALUES (:id_pokemon, :id_tipo)";
+                $stmt = $db->prepare($query);
+                $stmt->bindValue(':id_pokemon', $pokemonId);
+                $stmt->bindValue(':id_tipo', $tipo);
+                $stmt->execute();
+            } else {
+                echo "Tipo no válido: $tipo";
+            }
+        }
+
+        return $pokemonId;
+    }
+
+    public function modificatePokemon($id, $nombre = null, $numero = null, $tipos = [], $descripcion = null,  $imagen = null) {
+        $db = DB::getConexion();
+        $updates = [];
+        $params = [':id' => $id];
+
+        if (!is_null($nombre)) {
+            $updates[] = "nombre = :nombre";
+            $params[':nombre'] = $nombre;
+        }
+
+        if (!is_null($numero)) {
+            $updates[] = "numero = :numero";
+            $params[':numero'] = $numero;
+        }
+
+
+        if (!is_null($descripcion)) {
+            $updates[] = "descripcion = :descripcion";
+            $params[':descripcion'] = $descripcion;
+        }
+
+        if (!is_null($imagen)) {
+            $updates[] = "imagen = :imagen";
+            $params[':imagen'] = $imagen;
+        }
+
+        if (count($updates) > 0) {
+            $query = "UPDATE pokemon SET " . implode(', ', $updates) . " WHERE id = :id";
+            $stmt = $db->prepare($query);
+            $stmt->execute($params);
+        }
+
+        if (!empty($tipos)) {
+            $query = "DELETE FROM tipo_pokemon WHERE id_pokemon = :id";
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            foreach ($tipos as $tipo) {
+                $query = "INSERT INTO tipo_pokemon (id_pokemon, id_tipo) VALUES (:id_pokemon, :id_tipo)";
+                $stmt = $db->prepare($query);
+                $stmt->bindValue(':id_pokemon', $id);
+                $stmt->bindValue(':id_tipo', $tipo);
+                $stmt->execute();
+            }
+        }
+
+        return true;
+    }
+
+
 }
